7be04b088a5bc2642fe19e29043037d01bd4174b
belvip <pouadjeubelvi@gmail.com>
2024-10-16 12:38:53 +0100

    file update

--- src/App.jsx
+++ src/App.jsx
@@ -39,6 +39,7 @@ const router = createBrowserRouter([
         path: 'newsletter',
         element: <Newsletter />,
         action: newsletterAction,
+        errorElement: <SinglePageError />,
       },
       {
         path: 'about',
--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,34 +1,51 @@
 import React from 'react';
-import { Form } from 'react-router-dom';
+import { Form, redirect } from 'react-router-dom';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+const newsletterUrl = 'https://www.course-api.com/cocktails-newsletter';
 
 export const action = async ({ request }) => {
-    console.log(request);
-    
-    return null;
-}
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData);
+
+    try{
+        const response = await axios.post(newsletterUrl, data);
+        toast.success(response.data.msg);
+        
+        return redirect('/');
+    }catch(error){
+        console.log(error);
+        toast.error(error?.response?.data?.msg);
+        return error;
+    }
+
+};
 
 const Newsletter = () => {
+
     return (
 
-        <Form className="form">
+        <Form className="form" method="POST">
             <h4 style={{ textAlign: 'center', marginBottom: '2rem' }}>our newsletter</h4>
 
             {/* name */}
             <div className="form-row">
                 <label htmlFor='name' className='form-label'>name</label>
-                <input type='text' className='form-input' name='name' id='name' defaultValue="Belvinard" />
+                <input type='text' className='form-input' name='name' id='name' required />
             </div>
             {/* lastName */}
             <div className="form-row">
                 <label htmlFor='lastName' className='form-label'>last name</label>
-                <input type='text' className='form-input' name='lastName' id='lastName' defaultValue="Pachinco" />
+                <input type='text' className='form-input' name='lastName' id='lastName' required />
             </div>
             {/* name */}
             <div className="form-row">
                 <label htmlFor='email' className='form-label'>email</label>
-                <input type='text' className='form-input' name='email' id='email' defaultValue="pouadjeubelvi@gmail.com" />
+                <input type='text' className='form-input' name='email' id='email' required />
             </div>
             <button type='submit' className='btn btn-block' style={{ marginTop: '0.5rem'}}>submit</button>
+
         </Form>
     )
 }
